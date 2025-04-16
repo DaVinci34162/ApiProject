5fb41fec4cc39624d549c30cfa5fe84680064db1
DaVinci34162 <d.gisanura@alustudent.com>
2025-04-16 14:14:30 +0200

    Update server.js

--- Weatherapp/server.js
+++ Weatherapp/server.js
@@ -61,6 +61,6 @@ app.get('/forecast', async (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
